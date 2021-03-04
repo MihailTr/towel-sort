9e531015a76f93a2c6e82c45fafe35d90f9891c4
MihailTr <tmyfly@gmail.com>
2021-03-04 23:57:41 +0200

    feat: add algorithm

--- src/index.js
+++ src/index.js
@@ -2,5 +2,29 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+    if (matrix == undefined){
+        return [];
+    }
+    if (matrix.length == 0){
+        return [];
+    }
+    console.log(matrix.length);
+    let newMatrix = [];
+    for (let i = 0; i < matrix.length; i++){
+        if (i%2==0){
+            for (l of matrix[i]){
+                newMatrix.push(l);
+            }
+        }else{
+            for (r of matrix[i].reverse()){
+                newMatrix.push(r);
+            }
+        }
+    }
+    return newMatrix;
 }
+
+// console.log(towelSort([
+//     [1, 2],
+//     [3, 4],
+// ]))
